92d0c991ea791790f54f6537e1826cd640e19fb5
lethanhlongdvn <lethanhlong.dvn@gmail.com>
2026-01-25 22:08:55 +0700

    Update chat.js

--- Netlify/functions/chat.js
+++ Netlify/functions/chat.js
@@ -12,7 +12,7 @@ exports.handler = async (event, context) => {
     }
 
     // 1. Cấu hình Gemini API (Lấy từ Environment Variables trên Netlify)
-    const apiKey = process.env.GEMINI_API_KEY;
+    const apiKey = process.env.GOOGLE_API_KEY;
     if (!apiKey) {
       return {
         statusCode: 500,
