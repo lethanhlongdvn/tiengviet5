39011b4f99990beb7face79d86260415e8a0a602
lethanhlongdvn <lethanhlong.dvn@gmail.com>
2026-01-25 21:57:39 +0700

    Update firebase-logic.js

--- js/firebase-logic.js
+++ js/firebase-logic.js
@@ -5,7 +5,7 @@
 
 // Placeholder Configuration - PLEASE UPDATE WITH REAL CREDENTIALS IN NETLIFY/LOCAL
 const firebaseConfig = {
-    apiKey: "AIzaSyAXbN4-DsD-6-wOr-j2qcj5R5V2PAgqhaQ",
+    apiKey: "YOUR_FIREBASE_API_KEY",
     authDomain: "projectId.firebaseapp.com",
     projectId: "projectId",
     storageBucket: "projectId.appspot.com",
