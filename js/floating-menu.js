9d2cebcf257cf633ac94b60f3e15adee7f1dfea3
lethanhlongdvn <lethanhlong.dvn@gmail.com>
2026-01-29 21:53:21 +0700

    Update floating-menu.js

--- js/floating-menu.js
+++ js/floating-menu.js
@@ -30,7 +30,7 @@ const curriculumData = {
                     "id": 22, "name": "Tuần 22",
                     "lessons": [
                         { "title": "Bài 7: Đoàn thuyền đánh cá", "sections": [{ "title": "Đọc: Đoàn thuyền đánh cá", "url": "lesson_viewer.html?id=221-doan-thuyen-danh-ca" }, { "title": "LTVC: Luyện tập về câu ghép", "url": "lesson_viewer.html?id=221-ltvc" }, { "title": "Viết: Đánh giá, chỉnh sửa bài văn tả người", "url": "lesson_viewer.html?id=221-viet" }] },
-                        { "title": "Bài 8: Khu rừng của Mát", "sections": [{ "title": "Đọc: Khu rừng của Mát", "url": "lesson_viewer.html?id=222-khu-rung-cua-mat" }, { "title": "Viết: Viết bài văn tả người", "url": "lesson_viewer.html?id=222-viet" }] }
+                        { "title": "Bài 8: Khu rừng của Mát", "sections": [{ "title": "Đọc: Khu rừng của Mát", "url": "lesson_viewer.html?id=222-khu-rung-cua-mat" }, { "title": "Viết: Viết bài văn tả người", "url": "lesson_viewer.html?id=222-viet" }, { "title": "Nói và nghe: Những ý kiến khác biệt", "url": "lesson_viewer.html?id=222-nvn-nhung-y-kien-khac-biet" }] }
                     ]
                 }
             ]
