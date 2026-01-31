64977392d56a6705b7f9cb8dc75c1ccc5e5bcf7b
lethanhlongdvn <lethanhlong.dvn@gmail.com>
2026-01-31 20:20:00 +0700

    Delete list_keys.js

--- backup_2026_01_30/list_keys.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('data/lessons.js', 'utf8');
-// Mock window
-const window = {};
-eval(content);
-console.log(Object.keys(window.LESSON_DATA.lessons));
