c6f3ac60401e12520702886ddf052e4e8fe36ef8
lethanhlongdvn <lethanhlong.dvn@gmail.com>
2026-02-01 13:46:30 +0700

    Update lessons.js

--- data/lessons.js
+++ data/lessons.js
@@ -6,6 +6,12 @@
  */
 window.LESSON_DATA = {
     "lessons": {
+        "231-hoi-thoi-com-thi-o-dong-van": "week23/231-hoi-thoi-com-thi-o-dong-van.js",
+        "231-ltvc": "week23/231-ltvc.js",
+        "231-viet": "week23/231-viet.js",
+        "232-nhung-bup-che-tren-cay-co-thu": "week23/232-nhung-bup-che-tren-cay-co-thu.js",
+        "232-ltvc": "week23/232-ltvc.js",
+        "232-viet": "week23/232-viet.js"
 
 
 
